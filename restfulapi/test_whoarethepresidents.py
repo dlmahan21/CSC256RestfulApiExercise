150c9a19cc395e8ca10eb5159ba4d0bc12e5efd7
dlmahan21 <91039264+dlmahan21@users.noreply.github.com>
2021-10-24 16:54:49 -0400

    Create test_whoarethepresidents.py
    created a pytest module to analyze and verify api requests from DuckDuckGo

---
+++ restfulapi/test_whoarethepresidents.py
@@ -0,0 +1,20 @@
+import pytest
+import requests
+from whoarethepresidents import whoAreThePresidents
+
+url_ddg = "https://api.duckduckgo.com"
+def test_ddg0():
+    resp = requests.get(url_ddg + "/?q=DuckDuckGo&format=json")
+    rsp_data = resp.json()
+    assert "DuckDuckGo" in rsp_data["Heading"]
+
+def test_whoAreThePresidents():
+  the_presidents= ["Washington", "Adams", "Jefferson", "Madison", "Monroe", "Jackson", "Buren", "Harrison",
+                   "Tyler", "Taylor", "Lincoln", "Garfield", "Wilson", "Reagan", "Obama", "Trump", 
+                   "Polk", "Fillmore", "Pierce", "Buchanan", "Johnson", "Grant", "Hayes", "Arthur", 
+                   "Cleveland", "McKinley", "Roosevelt", "Taft", "Harding", "Coolidge", "Hoover", "Truman", 
+                   "Eisenhower", "Kennedy", "Nixon", "Ford", "Carter", "Bush", "Clinton", "Biden"]
+  presidents = whoAreThePresidents(url_ddg)
+  for last_name in the_presidents:
+    assert last_name in presidents
+  
